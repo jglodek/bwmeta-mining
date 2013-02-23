1bb6e9422f857b1e68688b7bf1bfe2d5ba0f4167
Jacek Głodek <jacek.glodek@gmail.com>
2013-02-23 14:02:33 +0100

    poprawka3

--- article-job/article_job.rb
+++ article-job/article_job.rb
@@ -1,3 +1,4 @@
+
 # encoding: utf-8
 require 'rubygems'
 require './configuration.rb'
@@ -17,24 +18,34 @@ def get_articles_from_xml(xml_string)
   get_elem_from_xml(xml_string, 'bwmeta1.level.hierarchy_Journal_Article')
 end
 
+$found = {}
 
 def scrap_article_data(article_xml, db)
   article = {}
   article[:legacy_id] = article_xml["id"]
 
   article_xml.children.each do |c|
+    case c.name
+      when "name"
+        article[:names] ||= []
+        article[:names].push({name: c.text, lang: c["lang"]})
+      when "attribute"
+        case c["key"]
+        when "bibliographical.description"
+          article[:description] = c["value"]
+        else
 
+          $found[c.name + " " + c["key"] ||= 0
+          $found[c.name + " " + c["key"]] += 1
+        end
+      else
+        $found[c.name] ||= 0
+        $found[c.name] += 1
+    end
   end
-  puts article
 end
 
-plik = 0
-plikow = Dir["./full_db/*.xml"].length
-
 Dir["./full_db/*.xml"].each do |file|
-  plik+=1
-  puts "plik #{plik}/#{plikow}" if plik%10==0
-
   xml_file = open(file)
 
   articles = get_articles_from_xml(xml_file.read)
@@ -45,6 +56,5 @@ Dir["./full_db/*.xml"].each do |file|
     scrap_article_data(article, db)
   end
 end
-puts JSON.pretty_generate(article_child)
 
-
+puts $found
